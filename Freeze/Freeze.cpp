15717c293b61f86ff3b7d9b7ddf77eb2361e7776
galang48 <galangyudha1945@gmail.com>
2024-03-05 12:24:24 +0700

    Add project files.

---
+++ Freeze/Freeze.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nBilangan;
+    string status;
+
+    srand(time(0));
+
+    nBilangan = rand() % 6;
+
+    if (nBilangan % 2 == 0) {
+        status = "Genap";
+    }
+    else {
+        status = "Ganjil";
+    }
+
+    cout << "bilangan adalah " << nBilangan << endl;
+    cout << "statusnya adalah " << status << endl;
+}
+
+
